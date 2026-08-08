ad63d9964c9c5446389630e836b57e03b6b3548b
danushka <danmadushzxc@gmail.com>
2026-08-08 21:03:09 +0530

    feat:import required modules

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../../core/auth/services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  login() : void {
+    const success = this.authService.login(
+      this.username,
+      this.password
+    );
+
+    if (success) {
+      this.router.navigate(['/app/dashboard']);
+      return;
+    }
+    this.errorMessage = 'Invalid username or password';
+  }
+}
